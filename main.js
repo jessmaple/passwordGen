2e2a6cc8e0c48db589e7e0ee7d5aa12c831c881f
Jessica Maple <maple@jessicas-mbp-4.attlocal.net>
2020-03-04 19:35:53 -0500

    dynamic variables

--- main.js
+++ main.js
@@ -10,3 +10,11 @@ var abcUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
    function generatePass(){
     var allChars = [];
     var resultPass = "";
+
+    var Totlength = prompt("How many characters would you like your password to be?");
+
+    if(Totlength <8 || Totlength > 128){
+        alert("It is recommended to have a password between 8 and 128 characters long!\nPlease start over.");
+    }
+
+    
